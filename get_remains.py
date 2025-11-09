07a91e5f593a0c9ee2f3fc887ef4352359594a7e
KirillKozlov <kirillandreevickozlov@yandex.ru>
2025-11-09 23:01:20 +0300

    update get_remain.py

--- get_remains.py
+++ get_remains.py
@@ -8,85 +8,86 @@ import logging
 from dotenv import load_dotenv
 import json 
 
-async def fetch_stocks(
-    wb_token: str,
-    session: aiohttp.ClientSession,
-    nm_ids: list
-):
-    """
-    Запрашивает остатки по заданным параметрам.
-    Возвращает JSON-ответ API.
-    """
-    headers = {
-        "Authorization": wb_token,
-        "Content-Type": "application/json"
-    }
-    now = datetime.now().strftime("%Y-%m-%d")
-    payload = {
-        "nmIDs": nm_ids,
-        "currentPeriod": {
-            "start": now,
-            "end": now
-        },
-        "stockType": "",
-        "skipDeletedNm": True,
-        "orderBy": {
-            "field": "stockCount",
-            "mode": "desc"
-        },
-        "availabilityFilters": [
-            "deficient", 
-            "actual",
-            "balanced",
-            "nonActual",
-            "nonLiquid",
-            "invalidData"
-        ],
-        "offset": 0
-    }
-    async with session.post(
-        url="https://seller-analytics-api.wildberries.ru/api/v2/stocks-report/products/products",
-        headers=headers,
-        json=payload
-    ) as resp:    
-        if resp.status == 429:
-            logging.warning("[WB API] Too many requests, retrying in 20s...")
-            await asyncio.sleep(22)
-            return await fetch_stocks(wb_token, session, nm_ids)
-        if resp.status != 200:
-            text = await resp.text()
-            raise RuntimeError(f"Ошибка API {resp.status}: {text}")
 
-        data = await resp.json()
-        return data
+class WbApi:
+    def __init__(self, token: str, redis: Redis):
+        self.token = token
+        self.redis = redis
 
+    async def fetch_stocks(
+        self,
+        session: aiohttp.ClientSession,
+        nm_ids: list
+    ):
+        """
+        Запрашивает остатки по заданным параметрам.
+        Возвращает JSON-ответ API.
+        """
+        headers = {
+            "Authorization": self.token,
+            "Content-Type": "application/json"
+        }
+        now = datetime.now().strftime("%Y-%m-%d")
+        payload = {
+            "nmIDs": nm_ids,
+            "currentPeriod": {
+                "start": now,
+                "end": now
+            },
+            "stockType": "",
+            "skipDeletedNm": True,
+            "orderBy": {
+                "field": "stockCount",
+                "mode": "desc"
+            },
+            "availabilityFilters": [
+                "deficient", 
+                "actual",
+                "balanced",
+                "nonActual",
+                "nonLiquid",
+                "invalidData"
+            ],
+            "offset": 0
+        }
+        async with session.post(
+            url="https://seller-analytics-api.wildberries.ru/api/v2/stocks-report/products/products",
+            headers=headers,
+            json=payload
+        ) as resp:    
+            if resp.status == 429:
+                logging.warning("[WB API] Too many requests, retrying in 20s...")
+                await asyncio.sleep(22)
+                return await self.fetch_stocks(session, nm_ids)
+            if resp.status != 200:
+                text = await resp.text()
+                raise RuntimeError(f"Ошибка API {resp.status}: {text}")
 
-
-async def load_nm_ids_and_amounts_to_redis(
-    redis_client: Redis,
-    nm_id_remains_count: dict,
-    REDIS_KEY_NM_IDS_REMAINS_HASH: str,
-    REDIS_KEY_NM_IDS_TITLES_HASH: str,  
-) -> None:
-    """
-    Загружает пары артикул-количество из Google Sheets в Redis с префиксом nm_id_in_articles_sheet
-    """
-
-    # Пропускаем заголовок
-    pipe = redis_client.pipeline(transaction=True)
+            data = await resp.json()
+            return data
 
 
-    for nm_id, data in nm_id_remains_count.items():
-        # сохраняем количество в хэш: nm_id=amount 
-        pipe.hset(REDIS_KEY_NM_IDS_REMAINS_HASH, nm_id, data["remains"])
-        # сохраняем название в хэш: nm_id=title 
-        pipe.hset(REDIS_KEY_NM_IDS_TITLES_HASH, nm_id, data["nm_id_name"])
+    async def load_nm_ids_and_amounts_to_redis(
+        self,
+        nm_id_remains_count: dict,
+        REDIS_KEY_NM_IDS_REMAINS_HASH: str,
+        REDIS_KEY_NM_IDS_TITLES_HASH: str,  
+    ) -> None:
+        """
+        Загружает пары артикул-количество из Google Sheets в Redis с префиксом nm_id_in_articles_sheet
+        """
 
-    await pipe.execute()
+        # Пропускаем заголовок
+        pipe = self.redis.pipeline(transaction=True)
 
-
-    logging.info(f"✅ Put {len(nm_id_remains_count)-1} nm_ids into Redis DB")
-    
+        for nm_id, data in nm_id_remains_count.items():
+            # сохраняем количество в хэш: nm_id=amount 
+            pipe.hset(REDIS_KEY_NM_IDS_REMAINS_HASH, nm_id, data["remains"])
+            # сохраняем название в хэш: nm_id=title 
+            pipe.hset(REDIS_KEY_NM_IDS_TITLES_HASH, nm_id, data["nm_id_name"])
+        await pipe.execute()
+        logging.info(f"✅ Put {len(nm_id_remains_count)-1} nm_ids into Redis DB")
+        
 async def main():
     load_dotenv()
     WB_TOKEN_STR = os.getenv("WB_TOKEN_STR")
@@ -94,15 +95,18 @@ async def main():
     REDIS_KEY_NM_IDS_REMAINS_HASH = os.getenv("REDIS_KEY_NM_IDS_REMAINS_HASH")
     REDIS_KEY_NM_IDS_TITLES_HASH = os.getenv("REDIS_KEY_NM_IDS_TITLES_HASH")
     
+    nm_ids = [508040605, 555620866, 518431572]  # ← подставьте ваши nmIDs
     redis = await asyncredis.from_url(REDIS_URL)
-    
-    nm_ids = [508040605]  # ← подставьте ваши nmIDs
+    api_client = WbApi(
+        token=WB_TOKEN_STR,
+        redis=redis
+    ) 
+
     
     result = {}
     async with aiohttp.ClientSession() as session:
         try:
-            api_response = await fetch_stocks(
-                wb_token=WB_TOKEN_STR,
+            api_response = await api_client.fetch_stocks(
                 session=session,
                 nm_ids=nm_ids
             )
@@ -118,13 +122,11 @@ async def main():
                     "nm_id_name": name
                 }
             # print(result)
-            await load_nm_ids_and_amounts_to_redis(
-                redis,
-                result,
-                REDIS_KEY_NM_IDS_REMAINS_HASH,
-                REDIS_KEY_NM_IDS_TITLES_HASH
+            await api_client.load_nm_ids_and_amounts_to_redis(
+                nm_id_remains_count=result,
+                REDIS_KEY_NM_IDS_REMAINS_HASH=REDIS_KEY_NM_IDS_REMAINS_HASH,
+                REDIS_KEY_NM_IDS_TITLES_HASH=REDIS_KEY_NM_IDS_TITLES_HASH
             )
-
             with open("data_statistics_per_nm_id.json", mode="w",encoding="utf-8") as f:
                 json.dump(result, f, indent=4, ensure_ascii=False)
         except Exception as e:
